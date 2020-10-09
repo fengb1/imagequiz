75c6ecf21d995faa340a33387c989cf2ae5382c2
fengb1 <fengb1@email.arizona.edu>
2020-10-09 08:04:35 -0700

    login page

--- imagequiz/src/components/Home.js
+++ imagequiz/src/components/Home.js
@@ -2,15 +2,61 @@ import React from 'react';
 import "./Home.css";
 
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      showLoginForm: false,
+      authenticated: false,
+    };
+  }
+
+  login = () => {
+    this.setState({showLoginForm: true});
+  }
+
+  onSubmit = (event) => {
+    if (this.state.username.trim().length > 0) {
+      this.setState({authenticated: true});
+    }
+    event.preventDefault();
+  }
+
+  onInputChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({[name]: value});
+  }
+
   render() {
-    return(
-      <div>
-        <div className="loginButton">
-          <button>Login</button>
+
+    if ((!this.state.authenticated) && this.state.showLoginForm) {
+      return(
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <label>Username:</label>
+            <input
+              name="username"
+              value={this.state.username}
+              onChange = {this.onInputChange}
+            ></input><br></br>
+            <button type="submit">Login</button>
+          </form>
         </div>
-        <div>Homepage</div>
-      </div>
-    );
+      );
+    }
+    else {
+      return(
+        <div>
+          <div className="loginButton">
+            {this.state.authenticated ? this.state.username :
+            <button onClick={this.login}>Login</button>}
+          </div>
+          <div>Homepage</div>
+        </div>
+      );
+    }
   }
 }
 
