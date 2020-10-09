65ddf23cb392ff704644459871ac3bd965ee2b49
fengb1 <fengb1@email.arizona.edu>
2020-10-09 08:05:34 -0700

    Updates

---
+++ precache-manifest.b331d240ff15781e24450d1e4963b779.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdb7f70875265963f9f13fbd2e34ba9",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "20c09b760a5a2f5ff1b7",
+    "url": "/imagequiz/static/css/main.a124e804.chunk.css"
+  },
+  {
+    "revision": "e3d8992116123d5fff3b",
+    "url": "/imagequiz/static/js/2.a068b877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.a068b877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c09b760a5a2f5ff1b7",
+    "url": "/imagequiz/static/js/main.374e49a3.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  }
+]);
